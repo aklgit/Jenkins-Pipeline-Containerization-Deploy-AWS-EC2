b77284666ac27e6a99db1c97d69b7a04a1d029a5
aklgit <50852948+aklgit@users.noreply.github.com>
2019-11-01 16:21:29 -0400

    Update deploy.sh

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -6,4 +6,5 @@ echo $PASS >> /tmp/.auth
 
 scp -i /opt/aklawsdeployjenkinsfile.pem /tmp/.auth ec2-user@0.00.000.00:/tmp/.auth
 scp -i /opt/aklawsdeployjenkinsfile.pem ./jenkins/deploy/publish ec2-user@0.00.000.00:/tmp/publish
+#ececute the script from remote
 ssh -i /opt/aklawsdeployjenkinsfile.pem  ec2-user@0.00.000.00 "/tmp/publish"
