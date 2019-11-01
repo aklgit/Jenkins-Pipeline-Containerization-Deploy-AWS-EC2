fca2727b5c1e26b242d6ea916d8f222d04884a0b
aklgit <50852948+aklgit@users.noreply.github.com>
2019-11-01 16:01:51 -0400

    Update deploy.sh

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/aklawsdeployjenkinsfile.pem /tmp/.auth ec2-user@3.88.133.57:/tmp/.auth
-scp -i /opt/aklawsdeployjenkinsfile.pem ./jenkins/deploy/publish ec2-user@3.88.133.57:/tmp/publish
-ssh -i /opt/aklawsdeployjenkinsfile.pem  ec2-user@3.88.133.57 "/tmp/publish"
+scp -i /opt/aklawsdeployjenkinsfile.pem /tmp/.auth ec2-user@0.00.000.00:/tmp/.auth
+scp -i /opt/aklawsdeployjenkinsfile.pem ./jenkins/deploy/publish ec2-user@0.00.000.00:/tmp/publish
+ssh -i /opt/aklawsdeployjenkinsfile.pem  ec2-user@0.00.000.00 "/tmp/publish"
